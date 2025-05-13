a75ce4e989d2d6662a10126f115c7ec89e0fb2e4
Igor Goldbeck de Aguiar <106772538+igaguiar@users.noreply.github.com>
2025-05-13 09:34:45 -0300

    criadas classes

---
+++ Datas/Aluno.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrabalhoFinalPOO.Datas
+{
+    public class Aluno
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string CPF { get; set; }
+        public string Email { get; set; }
+    }
+}
