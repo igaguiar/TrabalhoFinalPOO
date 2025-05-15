c0de93219aa15d1533c9de4cf1fe4d122c243294
Igor Goldbeck de Aguiar <106772538+igaguiar@users.noreply.github.com>
2025-05-15 12:10:24 -0300

    ajustes classes

--- Datas/Matricula.cs
+++ Datas/Matricula.cs
@@ -9,5 +9,7 @@ namespace TrabalhoFinalPOO.Datas
     public class Matricula
     {
         public int Id { get; set; }
+        public Aluno Aluno { get; set; }
+        public Turma Turma { get; set; }
     }
 }
--- Datas/Turma.cs
+++ Datas/Turma.cs
@@ -9,7 +9,11 @@ namespace TrabalhoFinalPOO.Datas
     public class Turma
     {
         public int Id { get; set; }
-        public string Nome { get; set; }
+        public int Ano { get; set; }
+        public int Semestre { get; set; }
         public int QuantidadeAlunos { get; set; }
+        public Curso Curso { get; set; }
+        public Professor Professor { get; set; }
+        public List<Aluno> Alunos { get; set; } = new List<Aluno>();
     }
 }
